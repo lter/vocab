4d0d1db050fb6ace53b79f1ee983fda8467a0379
jporter <jporter@6d1ce7c0-6df4-4690-9cef-b156bbbe68fc>
2012-07-30 20:12:03 +0000

    Correct typo to include brazilian LTER and modify name of spanish metacat

--- ILTER/multisearch.php
+++ ILTER/multisearch.php
@@ -15,14 +15,14 @@ if ($_REQUEST['relation']==""){$relation="narrowrelated";}
 
 print("<li><a href='querymetacat.php/".$_REQUEST['lang']."/en/".$relation."/".$_REQUEST['search']."'>Knowledge Network for Biocomplexity</a> or ");
 print("<a href='querymetacat.php/".$_REQUEST['lang']."/en/".$relation."/".$_REQUEST['search']."?metacat=http://metacat.lternet.edu/knb/metacat'>US LTER</a>  <font size=-2>Note: KNB and U.S. LTER servers are replicated</font>\n");
-print("<li><a href='querymetacat.php/".$_REQUEST['lang']."/es/".$relation."/".$_REQUEST['search']."?metacat=http://metacat.iecolab.es/knb/metacat'>Spain LTER</a>");
+print("<li><a href='querymetacat.php/".$_REQUEST['lang']."/es/".$relation."/".$_REQUEST['search']."?metacat=http://metacat.iecolab.es/knb/metacat'>Serra Nevada (Spain) LTER</a>");
 print("<li><a href='querymetacat.php/".$_REQUEST['lang']."/en/".$relation."/".$_REQUEST['search']."?metacat=http://db.cger.nies.go.jp/JaLTER/metacat/metacat'>JaLTER (search English keywords)</a>");
 print("<li><a href='querymetacat.php/".$_REQUEST['lang']."/ja/".$relation."/".$_REQUEST['search']."?metacat=http://db.cger.nies.go.jp/JaLTER/metacat/metacat'>JaLTER (search Japanese keywords)</a>");
 print("<li><a href='querymetacat.php/".$_REQUEST['lang']."/en/".$relation."/".$_REQUEST['search']."?metacat=http://myernet.frim.gov.my/FRIM/metacat'>MyERnet Forestry Institute of Malaysia</a>");
 print("<li><a href='querymetacat.php/".$_REQUEST['lang']."/zh-tw/narrowrelat".$_REQUEST['search']."ed/?metacat=http://metacat.tfri.gov.tw/tfri/metacat'>Taiwan Forestry Research Institute (searched using Chinese-traditional keywords)</a>");
 print("<li><a href='querymetacat.php/".$_REQUEST['lang']."/en/".$relation."/".$_REQUEST['search']."?metacat=http://metacat.tfri.gov.tw/tfri/metacat'>Taiwan Forestry Research Institute (searched using English keywords)</a>");
-	     break;
 print("<li><a href='querymetacat.php/".$_REQUEST['lang']."/pt/".$relation."/".$_REQUEST['search']."?metacat=http://ppbio.inpa.gov.br/knb/metacat'>PPBio Brazil</a>");
+	     break;
 ?>
 </ul>
 </body>
